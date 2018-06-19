30dc2b10b531ebb722f674bfd381a3ad99e16a13
TinaClient <40324887+TinaClient@users.noreply.github.com>
2018-06-19 22:07:04 +0800

    改动一波

--- Client/Assets/ArmOpCode/Editor/ArmOpCodeTools.cs
+++ Client/Assets/ArmOpCode/Editor/ArmOpCodeTools.cs
@@ -11,7 +11,8 @@ namespace SuperDreamArmTools
         [MenuItem("Arm反汇编工具/测试函数", false, 1)]
         public static void ArmToolTest()
         {
-            Debug.Log("ArmToolTest");
+            bool isOk = EditorUtility.DisplayDialog("测试函数", "这里是个测试函数", "ok", "cancel");
+
             Debug.Log("ArmToolTest isOk = " + isOk);
         }
     }
